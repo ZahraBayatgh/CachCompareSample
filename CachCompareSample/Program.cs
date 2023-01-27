a22c43bf941e9e3a95d89f16992c253376191d37
Zahra Bayat <58395585+ZahraBayatgh@users.noreply.github.com>
2023-01-27 17:03:49 +0330

    Add project files.

---
+++ CachCompareSample/Program.cs
@@ -0,0 +1,44 @@
+﻿using LazyCache;
+using Microsoft.Extensions.Caching.Memory;
+
+//MemoryCacheMethod();
+
+void MemoryCacheMethod()
+{
+    var cache = new MemoryCache(new MemoryCacheOptions());
+    var counter = 0;
+    object o=new object();
+
+    Parallel.ForEach(Enumerable.Range(1, 20), _ =>
+    {
+        var cachedItem = cache.GetOrCreate("key", _ =>
+        {
+            lock (o)
+            {
+                Thread.Sleep(1000);
+                return counter++;
+            } 
+        });
+        Console.Write($"{cachedItem} ");
+    });
+}
+
+LazyCacheMethod();
+void LazyCacheMethod()
+{
+    IAppCache cache = new CachingService();
+    var counter = 0;
+    object o = new object();
+    Parallel.ForEach(Enumerable.Range(1, 20), _ =>
+    {
+        var cachedItem = cache.GetOrAdd("key", _ =>
+        {
+            lock (o)
+            {
+                Thread.Sleep(1000); return counter++;
+            }
+        });
+
+        Console.Write($"{cachedItem} ");
+    });
+}
